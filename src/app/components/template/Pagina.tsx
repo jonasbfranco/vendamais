26b0cfeb4f217e787a555e0b08926818b8249d48
Jonas Baptista Franco <jonas@MacBook-Air-de-Jonas.local>
2025-02-26 14:23:40 -0300

    first commit

---
+++ src/app/components/template/Pagina.tsx
@@ -0,0 +1,13 @@
+
+export interface PaginaProps {
+    children: any
+    className?: string
+}
+
+export default function Pagina(props: PaginaProps) {
+    return (
+        <div>
+            <main className={`flex-1 p-7 ml-72 ${props.className ?? ''}`}>{props.children}</main>
+        </div>
+    )  
+}
